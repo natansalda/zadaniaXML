a65749e07d2be08e9e0bef028f3ecb6f0cc95e08
Natalia Nazaruk <nazarjazz@wp.pl>
2020-05-26 17:09:47 +0200

    Added comments to exercise 4 solution

--- app/src/main/java/com/example/zadaniaxml/MainActivity.kt
+++ app/src/main/java/com/example/zadaniaxml/MainActivity.kt
@@ -12,37 +12,46 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // set proper layout here
         setContentView(R.layout.activity_zadanie4_rozwiazanie)
     }
 
+    // changes color to blue
     fun makeBlue(view: View) {
         header_text.setTextColor(Color.BLUE)
     }
 
+    // changes color to red
     fun makeRed(view: View) {
         header_text.setTextColor(Color.RED)
     }
 
+    // changes color to green
     fun makeGreen(view: View) {
         header_text.setTextColor(Color.GREEN)
     }
 
+    // changes color to yellow
     fun makeYellow(view: View) {
         header_text.setTextColor(Color.YELLOW)
     }
 
+    // changes color to gray
     fun makeGray(view: View) {
         header_text.setTextColor(Color.GRAY)
     }
 
+    // changes color to white
     fun makeWhite(view: View) {
         header_text.setTextColor(Color.WHITE)
     }
 
+    // changes color to cyan
     fun makeCyan(view: View) {
         header_text.setTextColor(Color.CYAN)
     }
 
+    // changes color to black
     fun makeBlack(view: View) {
         header_text.setTextColor(Color.BLACK)
     }
