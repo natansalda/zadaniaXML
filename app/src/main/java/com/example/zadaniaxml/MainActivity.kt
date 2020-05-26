6ddf24ad5bff8fab1a702c2c509a0d68b3c6be2f
Natalia Nazaruk <nazarjazz@wp.pl>
2020-05-26 17:04:47 +0200

    Added exercise 4

--- app/src/main/java/com/example/zadaniaxml/MainActivity.kt
+++ app/src/main/java/com/example/zadaniaxml/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.zadaniaxml
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_zadanie1_rozwiazanie.*
+import kotlinx.android.synthetic.main.activity_zadanie4_rozwiazanie.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_zadanie2_rozwiazanie)
+        setContentView(R.layout.activity_zadanie4_rozwiazanie)
     }
 
-    fun pobierzText(view: View) {
-        var editText = edit_text.text.toString()
-        naglowek.text = editText
+    fun makeBlue(view: View) {
+        header_text.setTextColor(Color.BLUE)
     }
+
+    fun makeRed(view: View) {
+        header_text.setTextColor(Color.RED)
+    }
+
+    fun makeGreen(view: View) {
+        header_text.setTextColor(Color.GREEN)
+    }
+
+    fun makeYellow(view: View) {
+        header_text.setTextColor(Color.YELLOW)
+    }
+
+    fun makeGray(view: View) {
+        header_text.setTextColor(Color.GRAY)
+    }
+
+    fun makeWhite(view: View) {
+        header_text.setTextColor(Color.WHITE)
+    }
+
+    fun makeCyan(view: View) {
+        header_text.setTextColor(Color.CYAN)
+    }
+
+    fun makeBlack(view: View) {
+        header_text.setTextColor(Color.BLACK)
+    }
+
 
 }
